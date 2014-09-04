15b44c35979c9de887f03a0d643895892d825a08
Lorenzo Sfarra <lorenzosfarra@gmail.com>
2014-09-04 09:47:07 +0200

    JQuery POST AJAX

---
+++ JS/jqueryPOSTAJAX/test.php
@@ -0,0 +1,9 @@
+<?php
+  /**
+   * Author: Lorenzo Sfarra <lorenzosfarra@gmail.com>
+   *
+   * Simple example to show how to perform an AJAX POST request.
+   *
+   */
+  echo "nome inviato: " . $_POST['name'];
+?>
---
+++ README.md
@@ -0,0 +1,2 @@
+Simple samples of different languages and for different targets.
+Just an excuse to use the repo name "simple sample".
